697067799488e14fa09cf221124cdf487bf077c0
Mohamed Ait <51912091+dayofmythology@users.noreply.github.com>
2023-11-20 11:49:16 +0000

    create new dev vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,27 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-1a", "us-west-1b", "us-west-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id = module.blog_vpc.public_subnets [0]
   tags = {
     Name = "HelloWorld"
   }
@@ -33,7 +49,7 @@ module "blog_sg" {
   version = "5.1.0"
   name = "blog_new"
 
-  vpc_id = data.aws_vpc.default.id
+  vpc_id = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
@@ -42,10 +58,4 @@ module "blog_sg" {
   egress_cidr_blocks = ["0.0.0.0/0"]
 
 }
-
-resource  "aws_security_group" "blog"{
-  name = "my_tf_blog_sec_group"
-  description = "Terraform security group created for the blog. Allow HTTP/s in + Allow everything out"
-
-  vpc_id = data.aws_vpc.default.id
 }
